f8e9cb0bb0a09daa202e3cf2458d4ce4f9c7126c
Uzair2120 <asnuzarshad2004@gmail.com>
2023-03-14 13:57:09 +0500

    updated-14

--- css/index.js
+++ css/index.js
@@ -1,8 +1,8 @@
-const menu= document.querySelector(".button");
-const btn=document.querySelector(".in-btn");
-const menbar=document.querySelector(".menu");
+const menu = document.querySelector(".button");
+const btn = document.querySelector(".in-btn");
+const menbar = document.querySelector(".menu");
 
-menu.addEventListener('click', function(a){
+menu.addEventListener('click', function (a) {
     menu.classList.toggle("active");
     btn.classList.toggle("btn-active");
     menbar.classList.toggle("after-menu");
